5288f38d8f0af1a0fe3869341418b69c37787a03
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2025-06-25 14:40:32 -0300

    Update

--- Content/IrradiatedSavannaGrass.cs
+++ Content/IrradiatedSavannaGrass.cs
@@ -6,15 +6,18 @@ namespace ExampleSpiritCrossmod.Content;
 
 internal class IrradiatedSavannaGrass : SavannaGrass
 {
+    // For convenience, Savanna Grass includes a property to set map entry color.
     protected override Color MapColor => new(212, 255, 86);
 
     public override void SetStaticDefaults()
     {
         base.SetStaticDefaults();
 
-        SpiritSets.Mowable[Type] = -1; // Savanna Grass can be mowed, but this can't - remove mowability
+        SpiritSets.Mowable[Type] = -1; // Savanna Grass can be mowed, but this can't - remove mowability.
 
-        ExampleSpiritCrossmod.Reforged.Call("RegisterConversionTile", IrradiatedConversion.ConversionType, ModContent.TileType<SavannaGrass>(), Type);
+        // Register a tile conversion using Spirit conversion sets.
+        // Because Savanna Grass does NOT rely on anchors for conversion, we can include our custom conversion ID.
+        ExampleSpiritCrossmod.Reforged.Call("RegisterConversionSet", nameof(SavannaGrass), IrradiatedConversion.ConversionType, Type);
     }
 
     public override void RandomUpdate(int i, int j)
@@ -22,7 +25,7 @@ internal class IrradiatedSavannaGrass : SavannaGrass
         GrowTiles(i, j);
 
         // This automatically converts tiles in any cardinal direction randomly, much like a normal Corrupt or Crimson tile would.
-        SpreadHelper.ConversionSpread(i, j, IrradiatedConversion.ConversionType);
+        WorldGen.SpreadInfectionToNearbyTile(i, j, IrradiatedConversion.ConversionType);
     }
 
     protected override void GrowTiles(int i, int j)
@@ -33,13 +36,13 @@ internal class IrradiatedSavannaGrass : SavannaGrass
         {
             int grassChance = GrassAny() ? 6 : 35;
 
-            if (Main.rand.NextBool(grassChance)) // Grow either foliage tile randomly - Placer is an internal tool that makes it more convenient to grow/place stuff
-                Placer.PlaceTile<IrradiatedElephantGrass>(i, j - 1, Main.rand.Next(5, 8)).Send();
+            if (Main.rand.NextBool(grassChance)) // Grow either foliage tile randomly - Placer is an internal tool that makes it more convenient to grow/place stuff.
+                Placer.PlaceTile<IrradiatedElephantGrass>(i, j - 1, Main.rand.Next(5, 8)).Send(); //Appending Send is important because it syncs the tile change in multiplayer.
             else if (Main.rand.NextBool(15))
                 Placer.PlaceTile<IrradiatedSavannaFoliage>(i, j - 1).Send();
         }
 
-        bool GrassAny()
+        bool GrassAny() // Checks if there is any Irradiated Elephant Grass surrounding these coordinates.
         {
             int type = ModContent.TileType<IrradiatedElephantGrass>();
             return Framing.GetTileSafely(i - 1, j - 1).TileType == type || Framing.GetTileSafely(i + 1, j - 1).TileType == type;
